e8fbf16e6356229d285b491b43b13276b71b24a2
OneKekBer <iliaberezovsky227@gmail.com>
2024-01-07 11:38:53 +0100

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 
 function App() {
-  return (
-    <div>
 
+  const content = [
+    {
+      title: 'хлеб духмынй'
+    },
+    {
+      title: 'хлеб чесночный'
+    },
+    {
+      title: 'хлеб медовый'
+    }
+  ]
+
+  return (
+    <div className='w-full h-screen bg-slate-200'>
+      <div className='flex p-14 justify-between '>
+        {content.map((item, i) => {
+
+
+          return (
+            <div key={i} className='w-[300px] p-5 h-[500px] bg-slate-400 shadow-xl rounded-[20px]'>{item.title}</div>
+          )
+        })}
+      </div>
     </div>
   )
 }
